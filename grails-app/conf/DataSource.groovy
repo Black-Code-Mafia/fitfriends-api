74c248ed38a6abad2d428f24efdc11987f1c7c10
adjeibad <dennis.adjei-baah@wwt.com>
2015-03-29 20:32:15 -0500

    change datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,4 @@
-grails{
-    mongo{
-        connectionString = "mongodb://localhost/fitfriendsdb"
-    }
-}
+
 
 dataSource {
     pooled = true
@@ -23,9 +19,10 @@ hibernate {
 // environment specific settings
 environments {
     development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        grails{
+            mongo{
+                connectionString = "mongodb://localhost/fitfriendsdb"
+            }
         }
     }
     test {
@@ -35,28 +32,9 @@ environments {
         }
     }
     production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
-               jmxEnabled = true
-               initialSize = 5
-               maxActive = 50
-               minIdle = 5
-               maxIdle = 25
-               maxWait = 10000
-               maxAge = 10 * 60000
-               timeBetweenEvictionRunsMillis = 5000
-               minEvictableIdleTimeMillis = 60000
-               validationQuery = "SELECT 1"
-               validationQueryTimeout = 3
-               validationInterval = 15000
-               testOnBorrow = true
-               testWhileIdle = true
-               testOnReturn = false
-               jdbcInterceptors = "ConnectionState"
-               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+        grails{
+            mongo{
+                connectionString = "mongodb://fitbuddy:ranThEat@ds037387.mongolab.com:37387/heroku_app35329028"
             }
         }
     }
